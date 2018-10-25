e0dad5640d59383656e8554b26d9b59e63d20304
MIGUEL ANGEL PUENTES RAMIREZ <ma.puentes@sis.virtual.uniandes.edu.co>
2018-10-25 11:57:56 -0500

    Falta arreglar unas cosas

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -8,16 +8,24 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.security.PublicKey;
 import java.security.Security;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.Mac;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
@@ -35,7 +43,6 @@ public class Main {
 	public Socket sock;
 	public X509Certificate certificadoServidor;
 
-	public static final String POSICION = "41 24.2028,2 10.4418";
 
 	public final static String algoritmo1="RSA"; //RSA
 	public String algoritmo2="HMACMD5"; //HMACMD5
@@ -46,7 +53,7 @@ public class Main {
 	{
 
 		try {
-			sock = new Socket("localhost",4444);
+			sock = new Socket("localhost",5555);
 			w = new PrintWriter(sock.getOutputStream(), true );
 			r = new BufferedReader( new InputStreamReader( sock.getInputStream( )) );
 		} catch (Exception e) {
@@ -59,149 +66,140 @@ public class Main {
 	}
 
 
-	private void protocolo() {
+	private void protocolo()
+	{
 
 		String recibido = null;
-
 
 		escribir("HOLA");
 		leer();
 		escribir("ALGORITMOS:"+algoritmo3+":"+algoritmo1+":"+algoritmo2);
 		leer();
-		escribir("CERCLNT:");
 		Security.addProvider(new BouncyCastleProvider());
 		KeyPairGenerator keyGen;
 		try {
 			keyGen = KeyPairGenerator.getInstance(algoritmo1, "BC");
 			keyGen.initialize(1024);
-			llaves=keyGen.generateKeyPair();
+			llaves = keyGen.generateKeyPair();
 			byte[] b = Seguridad.generarCertificado(llaves).getEncoded();
-			//			System.out.println(Transformacion.transformar(b));
-			sock.getOutputStream().write( b);
-			sock.getOutputStream().flush();
+			String transformado = toHexString(b);
+			escribir(transformado);
 
+			String respuesta = leer();
+			System.out.println("Resp" + respuesta);
 
-			String respuesta=leer();
-			if(!respuesta.split(":")[0].equals("CERTSRV"))
+			if(!respuesta.equals("OK"))
 			{
 				cerrar();
 				System.out.println("Error, el servidor no reconoció el certificado recibido.");
 				return;
 			}
 
-			byte []c= new byte[520];
+			String certificado = leer();
+			System.out.println("Cert" + certificado);
 
-			sock.getInputStream().read(c,0,520);
+			byte [] arrr = new byte[520];
+			arrr = toByteArray(certificado);
+
 			CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+			InputStream in = new ByteArrayInputStream(arrr);
 
-			//System.out.println("parte error");
-			InputStream in = new ByteArrayInputStream(c);
-			//			System.out.println(Transformacion.transformar(c));
 			certificadoServidor = (X509Certificate)certFactory.generateCertificate(in);
 
 
-
-
-			if(certificadoServidor!=null)
+			if(certificadoServidor != null)
 			{
-				escribir("ESTADO:OK");
+				escribir("OK");
 			}
 			else
 			{
-				escribir("ESTADO:ERROR");
+				escribir("ERROR");
 				System.out.println("No recibió el certificado");
 			}
 
-			//Leer data
-			//leer();
-			respuesta=leer();
-			if(!respuesta.split(":")[0].equals("DATA"))
-			{
-				cerrar();
-				System.out.println("Error, no se puede leer la llave cifrada.");
-				return;
-			}
-
-
-			respuesta=respuesta.split(":")[1];
-
-			System.out.println(respuesta+".");
-
-
-			//			byte[]num1ciph= Transformacion.destransformar(respuesta);
-
-			//			String llaveSim=new String(SeguridadEjemploEjemplo.DescifrarAsimetrico(num1ciph, llaves.getPrivate(), algoritmo1));
-			//			System.out.println(llaveSim);  
-
-
-			//			System.out.println("llave: "+llaveSim);
-
-			/////Se descifra la LLAVE SIMETRICA y se envia de nuevo la llave al servidor
-			//			byte[] llaveSimet=SeguridadEjemploEjemplo.DescifrarAsimetrico(num1ciph, llaves.getPrivate(), algoritmo1);
-
-
-//			byte[] llaveCifrada = SeguridadEjemploEjemplo.cifrarAsimetrico(llaveSimet,certificadoServidor.getPublicKey() , algoritmo1);	
-
-			//			String  llaveExtraidaCifrada = Transformacion.transformar(llaveCifrada);
-
-
-			//			escribir("DATA:" + llaveExtraidaCifrada );
-
-			respuesta=leer();
-			if(!respuesta.split(":")[1].equals("OK"))
-			{
-				cerrar();
-				System.out.println("Error, no se pudo reenviar la llave simetrica.");
-				return;
-			}
-
-
-//			SecretKey llaveSimetrica = new SecretKeySpec(llaveSimet, algoritmo3);
+			String llaveSimetrica = leer();
+			System.out.println("llaveSimetrica: "+ llaveSimetrica);
 
 
 
+			byte[] descencriptada = DescifrarAsimetrico(toByteArray(llaveSimetrica),llaves,"RSA");
 
-			//Acá es lo de ACT SE ENVIA LA POSICION Y EL CODIGO hash
+			byte [] cifrada = cifrarAsimetrico(descencriptada, certificadoServidor.getPublicKey(), "RSA");
+
+			String cifradaEnString = toHexString(cifrada);
+
+			escribir(cifradaEnString);
+
+			String resp = leer();
+
+			String consulta = "35";
+
+			SecretKey llaveN = new SecretKeySpec(descencriptada, 0, descencriptada.length,algoritmo3);
+
+			Cipher c = Cipher.getInstance(algoritmo3); 
+			c.init(Cipher.ENCRYPT_MODE, llaveN); 
+			byte[] answ = c.doFinal(consulta.getBytes());
+			String answString = toHexString(answ);
+			escribir(answString);
+
+			Mac hmac = Mac.getInstance(algoritmo2);
+			hmac.init(llaveN);
+			byte[] bMac = hmac.doFinal(consulta.getBytes());
+			String hMacString = toHexString(bMac);
+
+			escribir(hMacString);
+
+			String r = leer();
 
 
-			//			String posicion=Transformacion.transformar((SeguridadEjemplo.cifrarSimetrico(llaveSimetrica, POSICION.getBytes(), algoritmo3)));
-			//			escribir("ACT1:" + posicion);
 
-
-			//			String resumenDigital=Transformacion.transformar(SeguridadEjemplo.cifrarAsimetrico(SeguridadEjemplo.digest(POSICION.getBytes(), llaveSimetrica, algoritmo2), llaves.getPrivate(), algoritmo1));
-			//			escribir("ACT2:" + resumenDigital);
-
-
-			recibido= leer();
-			if(!recibido.split(":")[1].equals("OK"))
-			{
-				cerrar();
-				System.out.println("Error en posicion, llave simetrica o resumen digital");
-				return;
-			}				
-			else
-				System.out.println("Se envió la posicion");
-			cerrar();	
-
-
-		}catch(Exception e){
+		}catch(Exception e)
+		{
 			e.printStackTrace();
 		}
 
 
 
-
-
 	}
 
-	private void escribir(String string) {
+	private void escribir(String string)
+	{
 		w.println(string);
 		System.out.println(string);
+	}
 
+	public String toHexString(byte[] array)
+	{
+		return DatatypeConverter.printHexBinary(array);
+	}
+	public byte[] toByteArray(String a)
+	{
+		return DatatypeConverter.parseHexBinary(a);
+	}
+
+	public static byte[] DescifrarAsimetrico (byte[] msg, KeyPair key , String alg) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,IllegalBlockSizeException, BadPaddingException
+	{
+		Cipher decifrador = Cipher.getInstance(alg); 
+		decifrador.init(Cipher.DECRYPT_MODE, key.getPrivate()); 
+		return decifrador.doFinal(msg);
+	}
+	public static byte[] cifrarAsimetrico (byte[] msg, KeyPair key , String algo) throws IllegalBlockSizeException, BadPaddingException, InvalidKeyException,NoSuchAlgorithmException, NoSuchPaddingException
+	{
+		Cipher decifrador = Cipher.getInstance(algo); 
+		decifrador.init(Cipher.ENCRYPT_MODE, key.getPublic()); 
+		return decifrador.doFinal(msg);
+	}
+
+	public static byte[] cifrarAsimetrico (byte[] msg, PublicKey key , String algo) throws IllegalBlockSizeException, BadPaddingException, InvalidKeyException,NoSuchAlgorithmException, NoSuchPaddingException
+	{
+		Cipher decifrador = Cipher.getInstance(algo); 
+		decifrador.init(Cipher.ENCRYPT_MODE, key); 
+		return decifrador.doFinal(msg);
 	}
 
 
-	private String leer() {
+	private String leer()
+	{
 		System.out.println("leyendo");
 		String m="";
 		try {
